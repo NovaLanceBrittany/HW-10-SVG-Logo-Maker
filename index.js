32f59fa64193dfd431930ff2557760c4cc173024
Brittany Jungclaus <164208757+NovaLanceBrittany@users.noreply.github.com>
2024-05-27 11:43:45 -0500

    the questions worked! yaya!

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-
+// Node Packages
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+// Calling in other files
+const Shapes = require('./lib/shapes');
 
-const generateSVG = require('./lib/shapes');
-
-
+// This is a Node Package that adds a conditionial requirement per developer digression. 
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
-
-
-
-
-inquirer-maxlength-input-prompt
-
 
 
 function SVGquestions() {
@@ -24,7 +18,7 @@ function SVGquestions() {
         type: "maxlength-input",
         message: "Welcome to the SVG Generator! Let's start by choosing  your short hand logo. Please enter up to 3 characters to use as your short hand logo.",
         name: "shorthand",
-        maxlength: 3,
+        maxLength: 3,
       },
       {
         type: "input", 
@@ -32,10 +26,10 @@ function SVGquestions() {
         name: "hextext"
       },
       {
-        type: "input", 
+        type: "list", 
         message: "What kind of shape would you like use for your logo? Choose from the selection below:",
         name: "shapeshape",
-        choices: ["Circle", "Triangle", "Square"]
+        choices: ["Circle", "Triangle", "Square", "Hexagon"]
       },
       {
         type: "input", 
@@ -46,7 +40,36 @@ function SVGquestions() {
   )
     .then((response) => {
 
-      fs.writeFile("./exampleSVG", generateSVG.generateSVG(response), function(err){
+
+      switch(response.shapeshape) {
+        case 'Circle':
+          response.shapeshape = new Shapes.Circle(response.hexshape);
+          break;
+        case 'Triangle':
+          response.shapeshape = new Shapes.Triangle(response.hexshape);
+          break;
+        case 'Square':
+          response.shapeshape = new Shapes.Square(response.hexshape);
+         break;
+        case 'Hexagon':
+          response.shapeshape = new Shapes.Hexagon(response.hexshape);
+          break;
+      }
+
+      console.log("Testing for shapes", response.shapeshape)
+
+      const result = 
+      `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+      ${response.shapeshape.render()}
+    
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.hextext}">${response.shorthand}</text>
+    
+      </svg>`
+
+
+      //This is the writeFile function that takes all of the information inputed by the user and generates an SVG File. 
+      fs.writeFile("./assets/exampleSVG/example.svg", result, function(err){
         if( err ) return console.log("writeFile is not working")
         console.log("SVG logo Generated Successfully!")
       })
